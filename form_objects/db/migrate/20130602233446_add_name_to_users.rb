c7c8e3585ada2b315c58de9266e69136a84e4cc7
Simmi Badhan <simmi@addvalsolutions.com>
2015-02-09 15:53:51 +0530

    changed base project

--- form_objects/db/migrate/20130602233446_add_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string
-  end
-end
--- form_objects/db/migrate/20130602233827_add_subscribed_at_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSubscribedAtToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :subscribed_at, :datetime
-  end
-end
