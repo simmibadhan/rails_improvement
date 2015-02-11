5f1e5060170e7a951abda6f77ac90ebd7c5080cd
Simmi Badhan <simmi@addvalsolutions.com>
2015-02-11 17:10:07 +0530

    before decorators

--- decorators/app/controllers/posts_controller.rb
+++ decorators/app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
 class PostsController < ApplicationController
+	def show
+		@post = Post.find_by_id(params[:id])
+	end
+	def index
+		@posts = Post.order("created_at DESC")
+	end
 end
--- decorators/app/models/post.rb
+++ decorators/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+
+	def is_front_page?
+		created_at > 2.days.ago
+	end
 end
